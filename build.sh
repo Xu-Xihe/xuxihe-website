784e35694f19ac826c850dffbb248e3947c9b06a
Xu-Xihe <xxhsishi@outlook.com>
2021-10-18 23:14:47 +0800

    auto

---
+++ build.sh
@@ -0,0 +1,17 @@
+git add *
+
+git commit -m "auto"
+
+git push
+
+hugo
+
+cd public
+
+git add *
+
+git commit -m "auto"
+
+git push
+
+read -s -n1 -p "按任意键继续 ... "
---
+++ start.sh
@@ -0,0 +1,7 @@
+git pull
+
+cd public
+
+git pull
+
+read -s -n1 -p "按任意键继续 ... "
