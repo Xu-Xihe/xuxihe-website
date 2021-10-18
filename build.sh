381008b825057ba637281981e7eb44b9da6e8364
Xu-Xihe <xxhsishi@outlook.com>
2021-10-18 23:35:46 +0800

    auto

--- build.sh
+++ build.sh
@@ -1,17 +1,9 @@
 git add *
-
 git commit -m "auto"
-
 git push
-
 hugo
-
 cd public
-
 git add *
-
 git commit -m "auto"
-
 git push
-
-read -s -n1 -p "按任意键继续 ... "
+read -s -h1 -p "Any key to continue..."
